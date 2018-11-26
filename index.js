58749928bb34222251be5167f83b88011b4f4491
Joe Peel <joepeel@Joes-MBP.default>
2018-11-26 13:20:47 +0000

    Refactor example route

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet')
 const port = 3000;
+const rootRouter = require('./routes/root.js')
 
 
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(helmet())
 
-app.get('/', (req, res) => {
-    res.json('Hello World')
-})
-
+app.get('/', rootRouter)
 
 
 app.listen(port, () => {console.log('Listening fds')})
